8df1a2586a8462ed2a4c39f615417cc04871a7ef
Jake <umadbroskye@gmail.com>
2022-04-04 08:51:03 -0500

    Created rooms

--- hw10/Assets/Scenes/boxScript.cs
+++ hw10/Assets/Scenes/boxScript.cs
@@ -2,27 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class boxScript : MonoBehaviour{
-    public float speed = 5.0f;
-    public float jumpAmount = 10.0f;
+//small change
+public class boxScript : MonoBehaviour
+{ 
+    public float speed = 2.0f;
     private Rigidbody rb;
 
-    void Start(){
+    // Start is called before the first frame update
+    void Start() //like a constructor
+    {
         rb = this.gameObject.GetComponent<Rigidbody>();
     }
 
-    void Update(){
-        Transform cTransform = Camera.main.transform; //get camera transform
-        Vector3 fMovement = cTransform.forward * Input.GetAxis("Vertical"); //set forward movement to what is "forward" according to the camera (if the cube flips, this script will still work)
-        Vector3 hMovement = cTransform.right * Input.GetAxis("Horizontal"); //similar to the above except with a different axis. Move relative to the camera, ignore position/rotation of the cube
-        Vector3 movement = Vector3.ClampMagnitude(fMovement + hMovement, 1); //clamp magnitute of fmovement + hmovement to 1
-        transform.Translate(movement * speed * Time.deltaTime, Space.World); //moves the cube
-
-        if (Input.GetKeyDown("space")){ //check if space is pressed
-            rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse); //adds an impulse force to the cube, this gives it a better jump
+    // Update is called once per frame
+    void Update()
+    {
+        //this is the game object that this script is attached to
+        
+        if (Input.GetKeyDown("up"))
+        {
+            rb.velocity = Vector3.forward * speed;
         }
-    }
-    void FixedUpdate(){
-        rb.AddForce(Physics.gravity * 2 * rb.mass); //proper jumps (making sure the cube falls down at a nice speed instead of floating/hanging in the air)
+        else if (Input.GetKeyDown("down"))
+        {
+            rb.velocity = Vector3.back * speed;
+        }
+        else if (Input.GetKeyDown("left"))
+        {
+            rb.velocity = Vector3.left * speed;
+        }
+        else if (Input.GetKeyDown("right"))
+        {
+            rb.velocity = Vector3.right * speed;
+        }
+        else if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = Vector3.up * speed;
+        }
     }
 }
